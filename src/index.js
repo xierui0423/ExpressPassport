cf5f47f1cec97e2599f05d153cae455b43df42c6
Ray Xie <xierui0423@gmail.com>
2016-11-11 17:04:53 +0800

    tested pg db with express

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@
 import express from 'express';
 import path from 'path';
 import exphab from 'express-handlebars';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+
+const conString = 'postgres://postgres:inter1908@localhost/pgtest';
 
 const app = express();
 const port = 3000;
@@ -30,14 +34,70 @@ const testMiddleware = (req, rsp, next) => {
   next();
 };
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.use(logRequestHeaderMiddleware);
 app.use(testMiddleware);
 
-app.get('/', (request, response) => {
-  response.render('home', {
-    name: 'John',
+app.get('/users', (req, rsp, next) => {
+  pg.connect(conString, (conError, client, done) => {
+    if (conError) {
+      return console.error('error at establishing connection', conError);
+    }
+
+    client.query('SELECT name, age FROM users', [], (dbErr, result) => {
+      // this done callback signals the pg driver that the
+      // connection can be closed or returned to the connection pool
+      done();
+
+      if (dbErr) {
+        // pass the error to the express error handler
+        return next(dbErr);
+      }
+
+      const users = result.rows;
+
+      if (!users.length) {
+        rsp.send('No users are available');
+      } else {
+        rsp.render('home', {
+          name: users[users.length - 1].name,
+        });
+      }
+
+      return undefined;
+    });
+
+    return undefined;
   });
-})
+});
+
+app.post('/user', (req, res, next) => {
+  const user = req.body;
+
+  pg.connect(conString, (conError, client, done) => {
+    if (conError) {
+      return console.error('error at establishing connection', conError);
+    }
+
+    client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], (dbErr) => {
+      // this done callback signals the pg driver that the
+      // connection can be closed or returned to the connection pool
+      done();
+
+      if (dbErr) {
+        // pass the error to the express error handler
+        return next(dbErr);
+      }
+
+      res.sendStatus(200);
+
+      return undefined;
+    });
+
+    return undefined;
+  });
+});
 
 // Error handler
 app.use((err, req, rsp, next) => {
